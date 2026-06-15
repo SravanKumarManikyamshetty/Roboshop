4cd6f8035312e5a4858a69789c27bbac9616b926
Sravan Kumar Manikyamshetty <sravansmini@Mac-2949.lan>
2026-06-15 16:01:28 -0400

    first program

---
+++ shell/roboshop.sh
@@ -0,0 +1,12 @@
+AMI=ami-0220d79f3f480ecf5
+
+
+instanceid=(aws ec2 run-instances \
+    --image-id $AMI \
+    --instance-type t3.micro \
+    --security-group-ids "roboshop-common" "roboshop-$1" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$1}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+    )
+    echo "Intance Id: $instanceid "
