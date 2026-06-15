e5551a57c59f17d1cb004dd34138457384600b6e
Sravan Kumar Manikyamshetty <sravansmini@Mac-2949.lan>
2026-06-15 18:30:51 -0400

    adding new

---
+++ shell/roboshop-v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z06319852Q2KMY0JQH97T"
+DOMAIN_NAME="sravan.site"
+
+if [ $# -ln 2 ]; then
+    echo " these need more than 1 argument"
+    exit 1
+else
+    if [ "$1" != "create" ] && [ "$1" != "delete" ]; then
+        echo " first argu always either create or delete "
+    else
+        echo " $1 "
+    fi
+    echo " number of arguments $#"
+fi
