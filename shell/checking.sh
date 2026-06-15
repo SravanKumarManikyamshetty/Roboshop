85a2e725f69aef168e5338933f9a71f7713b9739
Sravan Kumar Manikyamshetty <sravansmini@Mac-2949.lan>
2026-06-15 17:01:00 -0400

    checking file addinnsdg

--- shell/checking.sh
+++ shell/checking.sh
@@ -1,5 +1,5 @@
 INSTANCE_ID=(aws ec2 describe-instances \
         --filters "Name=tag:Name,Values=roboshop-$1" \
-        --query "Reservations[].Instances[].InstanceId" \
+        --query "Reservations[*].Instances[*].InstanceId" \
         --output text   ) 
 echo "$INSTANCE_ID"
